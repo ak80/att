2e93ab713fa73ad1553903a08d19d5b7f1a1725b
Alexander Koch <josef.koch@gmail.com>
2017-02-23 21:36:59 +0100

    Add tests for DateTools

--- testtools/src/main/java/org/ak80/att/LocalDateTools.java
+++ testtools/src/main/java/org/ak80/att/LocalDateTools.java
@@ -1,11 +1,24 @@
 package org.ak80.att;
 
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.concurrent.TimeUnit;
+
 /**
  * Tools for LocalDate(Time)
  */
 public class LocalDateTools {
 
-  // TODO
+  public static LocalDate $Today() {
+    return LocalDate.now();
+  }
 
+  public static LocalDate $Yesterday() {
+    return LocalDate.now().minus(1, ChronoUnit.DAYS);
+  }
+
+  public static LocalDate $Tomorrow() {
+    return LocalDate.now().plus(1, ChronoUnit.DAYS);
+  }
 
 }
--- testtools/src/test/java/org/ak80/att/LocalDateToolsTest.java
+++ testtools/src/test/java/org/ak80/att/LocalDateToolsTest.java
@@ -3,12 +3,34 @@ package org.ak80.att;
 import org.hamcrest.core.Is;
 import org.junit.Test;
 
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
+import static org.ak80.att.LocalDateTools.$Today;
+import static org.ak80.att.LocalDateTools.$Tomorrow;
+import static org.ak80.att.LocalDateTools.$Yesterday;
 import static org.hamcrest.CoreMatchers.nullValue;
+import static org.hamcrest.Matchers.is;
 import static org.hamcrest.core.IsNot.not;
 import static org.junit.Assert.assertThat;
 
 
 public class LocalDateToolsTest {
+
+  @Test
+  public void today_returns_date_now() {
+    assertThat($Today(),is(LocalDate.now()));
+  }
+
+  @Test
+  public void yesterday_returns_date_now_minus_one() {
+    assertThat($Yesterday(),is(LocalDate.now().minus(1, ChronoUnit.DAYS)));
+  }
+
+  @Test
+  public void tomorrow_returns_date_now_plus_one() {
+    assertThat($Tomorrow(),is(LocalDate.now().plus(1, ChronoUnit.DAYS)));
+  }
 
   @Test
   public void dummyCreateInstanceForLineCoverage() {
